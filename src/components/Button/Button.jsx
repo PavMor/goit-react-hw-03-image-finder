2cdbf838105079ab0dd96ccd944dbe3bad48cd91
Your Name <pavlo.morozenko@ukr.net>
2023-04-23 21:13:12 +0200

    fix1

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
-import { BiPlusMedical } from 'react-icons/bi';
 import { LoadButton } from './Button.styled';
 
 export const Button = ({ onNextFetch }) => {
   return (
     <LoadButton type="button" onClick={onNextFetch}>
-      Load more <BiPlusMedical /* className={css.icon} */ />
+      Load more 
     </LoadButton>
   );
 }
